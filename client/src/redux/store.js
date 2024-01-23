4ec065f4c00b942d312edda0949fcd838e3958ce
babukre1 <108317783+babukre1@users.noreply.github.com>
2024-01-23 20:55:49 +0300

    Add redux persis

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./counter";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const rootReducer = combineReducers({ user: userReducer });
+
+const persisiConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+const persistedReducer = persistReducer(persisiConfig, rootReducer);
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
+
+export const persistor = persistStore(store);
