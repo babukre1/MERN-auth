d64e200e6d7f82edfa68555753b5ffeeb1a58934
babukre1 <108317783+babukre1@users.noreply.github.com>
2024-02-09 20:58:06 +0300

    .

--- api/index.js
+++ api/index.js
@@ -2,17 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
-import membersRoutes from "./routes/member.route.js";
-import adminRoutes from "./routes/admin.route.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
 
 const app = express();
 app.use(cookieParser());
 dotenv.config();
-app.use(cors());
 mongoose
   .connect(process.env.MONGO)
   .then((res) => {
@@ -24,12 +20,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/client/dist')));
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/members", membersRoutes);
-app.use("/api/admin", adminRoutes);
 
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
 app.use((err, req, res, next) => {
